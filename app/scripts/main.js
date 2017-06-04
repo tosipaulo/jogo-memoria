df627217059195c4464c9c9c44dea5564db00c45
Paulo Tosi <tosi.paulo@gmail.com>
2017-06-04 19:05:01 -0300

    Criando um estilo teste cards

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('\'Allo \'Allo!');
+(function(){
+	'use strict'
+
+	var card = document.querySelector('.card');
+	var contentCard = document.querySelector('.game__card');
+
+	card.addEventListener('click', flipCard, false)
+
+	function flipCard() {
+		card.classList.toggle('flipped');
+	}
+
+	var templateCard = [
+		'<div class="card">',
+          '<div class="face back"></div>',
+          '<div class="face front"></div>',
+        '</div>',
+	].join('');
+
+	for(var i = 0; i <= 24; i++ ) {
+		contentCard.appendChild(templateCard);
+	}
+
+})()
